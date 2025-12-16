1287be03eb3bd345e3def2510fe3a7bf6a6baf4f
Estela-Dev <76627168+Estela-Dev@users.noreply.github.com>
2025-12-16 15:07:24 +0000

    Create BankAccountDemo.java

---
+++ exercises/week01/BankAccountDemo.java
@@ -0,0 +1,9 @@
+public class BankAccountDemo {
+
+    public static void main(String[] args) {
+
+        BankAccount account = new BankAccount("PT123", 100);
+
+        account.printAccountDetails();
+    }
+}
