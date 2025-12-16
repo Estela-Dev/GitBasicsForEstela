6bdb7620180d838bfd1add995762ff37ae913d0d
Estela-Dev <76627168+Estela-Dev@users.noreply.github.com>
2025-12-16 14:30:34 +0000

    Update Day01Basics.java

--- exercises/week01/Day01Basics.java
+++ exercises/week01/Day01Basics.java
@@ -1,1 +1,28 @@
+public class Day01Basics {
 
+    public static void main(String[] args) {
+        int amount = 100;
+        boolean isValid = isValidAmount(amount);
+
+        System.out.println("Amount: " + amount);
+        System.out.println("Is valid: " + isValid);
+
+        printTransactions();
+    }
+
+    public static boolean isValidAmount(int amount) {
+        if (amount > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void printTransactions() {
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("Transaction " + i);
+        }
+    }
+}
+
+day02: add conditional logic and loop examples
